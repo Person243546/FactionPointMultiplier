fa26d9af65d136f14861c57f13a0356e1f98c896
Person243546 <72211112+Person243546@users.noreply.github.com>
2025-03-17 18:41:34 +0000

    Add files via upload

---
+++ Source/FactionPointMultiplier/FactionPointMultiplier/FactionPointMultiplierPatch.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Verse;
+using UnityEngine;
+using RimWorld;
+using static FactionPointMultiplier.Startup;
+
+namespace FactionPointMultiplier
+{
+    class FactionPointMultiplierSettings : ModSettings
+    {
+
+        public Dictionary<string, float> factionandmultiplier = new Dictionary<string, float>();
+        public List<string> factionlist1 = new List<string>();
+        public List<float> multiplierlist2 = new List<float>();
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref factionandmultiplier, "factionandmultiplier", LookMode.Value, LookMode.Value, ref factionlist1, ref multiplierlist2);
+        }
+
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard ls = new Listing_Standard();
+            ls.Begin(inRect);
+            ls.GapLine();
+            ls.Label("Faction Point Multipliers:");
+            for (var i = 0; i < allFactions.Length; i++)
+            {
+                var name = allFactions[i].defName;
+                var value = FactionPointMultiplierMod.settings.factionandmultiplier[name];
+                string buf = value.ToString();
+                ls.TextFieldNumericLabeled<float>(name, ref value, ref buf, 0.01f, 100f);
+                FactionPointMultiplierMod.settings.factionandmultiplier[name] = value;
+            }
+            ls.End();
+        }
+    }
+
+    class FactionPointMultiplierMod : Mod
+    {
+        public static FactionPointMultiplierSettings settings;
+        public FactionPointMultiplierMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<FactionPointMultiplierSettings>();
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoSettingsWindowContents(inRect);
+        }
+        public override string SettingsCategory()
+        {
+            return "FactionPointMultiplier".Translate();
+        }
+    }
+}
---
+++ Source/FactionPointMultiplier/FactionPointMultiplier/Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace FactionPointMultiplier
+{
+    [HarmonyPatch(typeof(IncidentWorker_Raid))]
+    [HarmonyPatch("AdjustedRaidPoints")]
+    class FactionPointMultiplierPatch
+    {
+        static void Prefix(ref float points, ref Faction faction)
+        {
+            if (FactionPointMultiplierMod.settings.factionandmultiplier.ContainsKey(faction.def.defName))
+            {
+                points *= FactionPointMultiplierMod.settings.factionandmultiplier[faction.def.defName];
+            }
+        }
+    }
+}
